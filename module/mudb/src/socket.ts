f11985b2778a9ec6b01574fd90f380e7b967962a
He Diyi <esdoppio@gmail.com>
2018-09-20 20:42:25 +0800

    core: add MuMultiSocketServer

--- module/mudb/src/socket.ts
+++ module/mudb/src/socket.ts
@@ -47,3 +47,74 @@ export interface MuSocketServer {
     start(spec:MuSocketServerSpec);
     close();
 }
+
+export class MuMultiSocketServer implements MuSocketServer {
+    private _state = MuSocketServerState.INIT;
+
+    get state () : MuSocketServerState {
+        return this._state;
+    }
+
+    public clients:MuSocket[] = [];
+
+    private _servers:MuSocketServer[];
+    private _numOnlineServers = 0;
+
+    private _onready:MuReadyHandler = () => { };
+    private _onclose:MuCloseHandler = () => { };
+
+    constructor (servers:MuSocketServer[]) {
+        this._servers = servers;
+    }
+
+    public start (spec:MuSocketServerSpec) {
+        this._onready = spec.ready;
+        this._onclose = spec.close;
+
+        for (let i = 0; i < this._servers.length; ++i) {
+            this._servers[i].start({
+                ready: () => {
+                    ++this._numOnlineServers;
+                },
+                connection: spec.connection,
+                close: () => {
+                    --this._numOnlineServers;
+                },
+            });
+        }
+
+        const startHandle = setInterval(
+            () => {
+                if (this._numOnlineServers < this._servers.length) {
+                    return;
+                }
+
+                clearInterval(startHandle);
+
+                this._state = MuSocketServerState.RUNNING;
+                this._onready();
+            },
+            300,
+        );
+    }
+
+    public close () {
+        for (let i = 0; i < this._servers.length; ++i) {
+            this._servers[i].close();
+        }
+
+        const closeHandle = setInterval(
+            () => {
+                if (this._numOnlineServers > 0) {
+                    return;
+                }
+
+                clearInterval(closeHandle);
+
+                this._state = MuSocketServerState.SHUTDOWN;
+                this._onclose();
+            },
+            300,
+        );
+    }
+}
